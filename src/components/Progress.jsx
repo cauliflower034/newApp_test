d34cc832b8132548c64a3a141cecdef553c2315d
Shingo <93265847+cauliflower034@users.noreply.github.com>
2022-03-04 17:30:03 +0900

    不必要なファイルのインポート削除

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { BaseButton } from "./Object/Button/BaseButton";
 import { PrimaryButton } from "./Object/Button/PrimaryButton";
 import { ProgressBar } from "./Object/ProgressBar";
 
